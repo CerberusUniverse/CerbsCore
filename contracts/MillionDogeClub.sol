d583ae3cba2560fe328e8b6f022262184e233108
tongyu <476871232@qq.com>
2022-08-15 10:35:12 +0800

    update

--- contracts/MillionDogeClub.sol
+++ contracts/MillionDogeClub.sol
@@ -1,6 +1,6 @@
 //SPDX-License-Identifier: Unlicense
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.2;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
--- contracts/interfaces/IMillionDogeClub.sol
+++ contracts/interfaces/IMillionDogeClub.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.6;
+pragma solidity ^0.8.2;
 
 import "@openzeppelin/contracts/interfaces/IERC721.sol";
 
